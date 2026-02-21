7b0ca80cc8cdda539a50a260ef4a1a63382530fd
WottTheScoz <josephtorres611@gmail.com>
2026-02-20 23:32:36 -0500

    Pan Up Implemented
    - Make sure Main Camera has an animator component with the Camera animation controller.
    - Make sure you set up the correct Before Fade Out () event in the FadeScreen component of ScaleGoal (it should be Main Camera.Animator.SetTrigger(PanUp)).
    - Camera will now pan upwards after completing the level. You can also control whether you want a fade in/out when the level begins/ends in FadeScreenAnimator.cs.

--- Assets/Scripts/Animator/FadeScreenAnimator.cs
+++ Assets/Scripts/Animator/FadeScreenAnimator.cs
@@ -1,19 +1,47 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 public class FadeScreenAnimator : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [Space(20)]
-    [SerializeField] UnityEvent OnFadeOut;
+    [SerializeField] bool performFadeIn;
+    [Tooltip("If unchecked, both event sets will be called, but the fade out animation won't play.")]
+    [SerializeField] bool performFadeOut;
+    [SerializeField] UnityEvent beforeFadeOut;
+    [SerializeField] UnityEvent onFadeOut;
+
+    void Start()
+    {
+        if(performFadeIn)
+        {
+            animator.SetTrigger("FadeIn");
+        }
+    }
 
     public void FadeOut()
     {
-        animator.Play("FadeOut");
+        beforeFadeOut?.Invoke();
+
+        if(performFadeOut)
+        {
+            animator.Play("FadeOut");   
+        }
+        else
+        {
+            StartCoroutine(ReplaceFadeOutTime());
+        }
     }
 
     public void AfterFadeOut()
     {
-        OnFadeOut?.Invoke();
+        onFadeOut?.Invoke();
+    }
+
+    IEnumerator ReplaceFadeOutTime()
+    {
+        yield return new WaitForSeconds(0.5f);
+        AfterFadeOut();
     }
 }
